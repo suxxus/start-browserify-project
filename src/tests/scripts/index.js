4bf9028a605ed0b130db3473d583c5badf88a1a7
suxxus <suxxus@outlook.com>
2016-10-09 14:40:02 +0200

    updated lint configuration

--- src/tests/scripts/index.js
+++ src/tests/scripts/index.js
@@ -1,9 +1,12 @@
-import test from 'tape';
 import main from 'scripts';
+import test from 'tape';
 
-test('index', function(t) {
-    var actual = main(),
+test('index', t => {
+
+    const actual = main(),
         expect = true;
+
     t.ok(actual, expect);
     t.end();
+
 });
